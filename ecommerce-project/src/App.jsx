265e97cee01f4a1623cd38621f7b9f7ba8afcd3e
Sanrose <sanrosebhetuwal@gmail.com>
2026-07-18 12:16:30 +0545

    Moved Orders And Tracking page from plain html css to react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,8 @@
 import {Routes, Route} from 'react-router'
 import { CheckoutPage } from './pages/CheckoutPage'
 import { HomePage } from './Pages/HomePage'
+import { TrackingPage } from './pages/TrackingPage'
+import { OrdersPage } from './pages/OrdersPage'
 import './App.css'
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/tracking" element={<TrackingPage />} />
       
     </Routes>
   )
